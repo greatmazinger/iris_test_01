ac94785361089502a2bab89376a37b458971a763
Raoul Veroy <rveroy@cs.tufts.edu>
2015-08-12 01:31:25 -0400

    Replacing tabs with spaces. Because tabs are evil.

--- src/net/veroy/analysis/IrisTest01.java
+++ src/net/veroy/analysis/IrisTest01.java
@@ -18,46 +18,46 @@ import org.deri.iris.compiler.ParserException;
  */
 public class IrisTest01 {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Configuration config = KnowledgeBaseFactory.getDefaultConfiguration();
-		Parser parser = new Parser();
-		IKnowledgeBase kbase;
-		String program = "man('homer').\n" +
-						 "woman('marge').\n" +
-						 "hasSon('homer','bart').\n" +
-						 "isMale(?x) :- man(?x).\n" +
-						 "isFemale(?x) :- woman(?x).\n" +
-						 "isMale(?y) :- hasSon(?x,?y).\n" +
-						 "?-isMale(?x).";
-		try {
-			parser.parse(program);
-		} catch (ParserException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			 kbase = KnowledgeBaseFactory.createKnowledgeBase( parser.getFacts(),
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Configuration config = KnowledgeBaseFactory.getDefaultConfiguration();
+        Parser parser = new Parser();
+        IKnowledgeBase kbase;
+        String program = "man('homer').\n" +
+                         "woman('marge').\n" +
+                         "hasSon('homer','bart').\n" +
+                         "isMale(?x) :- man(?x).\n" +
+                         "isFemale(?x) :- woman(?x).\n" +
+                         "isMale(?y) :- hasSon(?x,?y).\n" +
+                         "?-isMale(?x).";
+        try {
+            parser.parse(program);
+        } catch (ParserException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+             kbase = KnowledgeBaseFactory.createKnowledgeBase( parser.getFacts(),
                                                                parser.getRules(),
                                                                config );
-		} catch (EvaluationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
-		}
-		List<IQuery> queryList = parser.getQueries();
-		for (Iterator<IQuery> iter = queryList.iterator(); iter.hasNext(); ) {
-		    IQuery query = iter.next();
-			try {
-				kbase.execute(query);
-			} catch (EvaluationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				return;
-			}
-		}
-	}
+        } catch (EvaluationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return;
+        }
+        List<IQuery> queryList = parser.getQueries();
+        for (Iterator<IQuery> iter = queryList.iterator(); iter.hasNext(); ) {
+            IQuery query = iter.next();
+            try {
+                kbase.execute(query);
+            } catch (EvaluationException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
 }
