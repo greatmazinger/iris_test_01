3187bdd663310976c947d97e39a4da1267ed2af9
Raoul Veroy <raoul@littletechtips.com>
2015-08-24 02:09:18 -0400

    Test code for points-to dynamic analysis

--- src/net/veroy/analysis/IrisTest01.java
+++ src/net/veroy/analysis/IrisTest01.java
@@ -26,13 +26,43 @@ public class IrisTest01 {
         Configuration config = KnowledgeBaseFactory.getDefaultConfiguration();
         Parser parser = new Parser();
         IKnowledgeBase kbase;
-        String program = "man('homer').\n" +
-                         "woman('marge').\n" +
-                         "hasSon('homer','bart').\n" +
-                         "isMale(?x) :- man(?x).\n" +
-                         "isFemale(?x) :- woman(?x).\n" +
-                         "isMale(?y) :- hasSon(?x,?y).\n" +
-                         "?-isMale(?x).";
+        // String program = "man('homer').\n" +
+        //                  "woman('marge').\n" +
+        //                  "hasSon('homer','bart').\n" +
+        //                  "isMale(?x) :- man(?x).\n" +
+        //                  "isFemale(?x) :- woman(?x).\n" +
+        //                  "isMale(?y) :- hasSon(?x,?y).\n" +
+        //                  "?-isMale(?x).";
+        String program = // "pointsTo('A3',2,9).\n" +
+                         "pointsTo('A1','A2',0,6).\n" +
+                         "pointsTo('A3','A2',2,9).\n" +
+                         "pointsTo('A2','A4',2,7).\n" +
+                         "pointsTo('A5','A1',1,5).\n" +
+                         "pointsTo('A1','A4',7,9).\n" +
+                         "pointsTo('A3','A4',10,14).\n" +
+                         "timestamp(0).\n" +
+                         "timestamp(1).\n" +
+                         "timestamp(2).\n" +
+                         "timestamp(3).\n" +
+                         "timestamp(4).\n" +
+                         "timestamp(5).\n" +
+                         "timestamp(6).\n" +
+                         "timestamp(7).\n" +
+                         "timestamp(8).\n" +
+                         "timestamp(9).\n" +
+                         "timestamp(10).\n" +
+                         "timestamp(11).\n" +
+                         "timestamp(12).\n" +
+                         "timestamp(13).\n" +
+                         "timestamp(14).\n" +
+                         "pointsToInstant(?A,?B,?T) :- timestamp(?T), pointsTo(?A,?B,?S,?E), ?T >= ?S, ?E >= ?T.\n" +
+                         // TODO These are queries that worked. I used them to eventually develop the query I wanted.
+                         // "?-pointsTo('A3',?Z1,?ST3,?ET3),pointsTo('A1', ?Z2, ?ST1, ?ET1),?Z1=?Z2,?ST3>=?ST1.\n" +
+                         /// "?-pointsToInstant('A1',?Z1,?T).\n" +
+                         // "?-timestamp(?T), pointsToInstant('A3',?Z, ?T), pointsToInstant('A1', ?Z, ?T).\n" +
+                         // "?-timestamp(?T), pointsToInstant(?X ,?Z, ?T), pointsToInstant(?Y, ?Z, ?T).\n" +
+                         // TODO: End
+                         "?-timestamp(?T), pointsToInstant(?X ,?Z, ?T), pointsToInstant(?Y, ?Z, ?T), ?X != ?Y.\n";
         try {
             parser.parse(program);
         } catch (ParserException e) {
@@ -49,6 +79,7 @@ public class IrisTest01 {
             return;
         }
         List<IQuery> queryList = parser.getQueries();
+        int num = 0;
         for (Iterator<IQuery> iter = queryList.iterator(); iter.hasNext(); ) {
             IQuery query = iter.next();
             IRelation result;
@@ -59,9 +90,11 @@ public class IrisTest01 {
                 e.printStackTrace();
                 return;
             }
+            System.out.println(String.format("Query %d:", num));
             for (int i = 0; i < result.size(); ++i) {
                 System.out.println(result.get(i).toString());
             }
+            num += 1;
         }
     }
 }
